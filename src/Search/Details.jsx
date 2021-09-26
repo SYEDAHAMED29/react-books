6e3abc4636948a81ad289bb7d761ed66836e9afe
Syed Ahamed <SYEDAHAMED29>
2021-09-26 18:15:20 +0530

    Search page completed

--- src/Search/Details.jsx
+++ src/Search/Details.jsx
@@ -2,35 +2,60 @@ import React from "react";
 import Button from "@mui/material/Button";
 import {makeStyles} from "@material-ui/core";
 import {Grid, Typography} from "@mui/material";
+import {useState} from "react";
 
 const useStyle = makeStyles({
-  gridGap: {
-    margin: "1rem",
-    padding: "1rem",
-  },
+   gridGap: {
+      margin: ".5rem",
+      padding: ".5rem",
+   },
+   btn: {
+      backgroundColor: "red",
+      margin: "1rem 0",
+   },
+   link: {
+      cursor: "pointer",
+      color: "blue",
+   },
 });
 
 function Details(props) {
-  const classes = useStyle();
+   const classes = useStyle();
+   const [read, setRead] = useState(false);
 
-  return (
-    <Grid item sm={12} md={4} lg={3} className={classes.gridGap}>
-      <Typography variant="h4">{props.title}</Typography>
-      <img src={props.image} alt="Book image" />
-      <Typography variant="h6">{props.author}</Typography>
-      <Typography variant="p">{props.description}</Typography>
-      {props.buy && (
-        <Button
-          variant="contained"
-          color="info"
-          href={props.buy}
-          target="_blank"
-        >
-          Buy Now
-        </Button>
-      )}
-    </Grid>
-  );
+   return (
+      <Grid item sm={12} md={3} lg={3} className={classes.gridGap}>
+         {/* //?Added the title, book image and author */}
+         <Typography variant="h4">{props.title}</Typography>
+         <img src={props.image} alt="Book" />
+         <Typography variant="h6">{props.author}</Typography>
+
+         {/* //?Added the read more */}
+         <Typography variant="p">
+            {props.description.toString().slice(0, 100)}
+            <p onClick={() => setRead(true)}>
+               {read ? (
+                  props.description
+               ) : (
+                  <span className={classes.link}>{props.description && "...Read more"}</span>
+               )}
+            </p>
+         </Typography>
+
+         {/* //?Added the buy button and price */}
+         {props.buy && (
+            <Button
+               className={classes.btn}
+               variant="contained"
+               color="info"
+               href={props.buy}
+               target="_blank"
+            >
+               Buy ₹{props.price}
+            </Button>
+         )}
+      </Grid>
+   );
 }
 
 export default Details;
--- src/Search/Search.jsx
+++ src/Search/Search.jsx
@@ -8,87 +8,79 @@ import Details from "./Details";
 const apiKey = "AIzaSyDEYdTddq5aVsLK8YzyTTS7VG5RPlEJ9S4";
 
 function Search() {
-  const [book, setBook] = useState("");
-  const [result, setResult] = useState([]);
+   const [book, setBook] = useState("");
+   const [result, setResult] = useState([]);
 
-  async function loadData() {
-    axios
-      .get(
-        "https://www.googleapis.com/books/v1/volumes?q=" +
-          book +
-          "&key=" +
-          apiKey
-      )
-      .then(data => {
-        console.log(data.data.items);
-        setResult(data.data.items);
-      })
-      .catch(err => {
-        window.alert("Please provide a valid book name");
-        window.location.replace("/search");
-      });
-  }
+   async function loadData() {
+      try {
+         const response = await axios.get(
+            "https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey
+         );
 
-  function handleClick(e) {
-    e.preventDefault();
-    loadData();
-  }
-  return (
-    <Container>
-      <Typography variant="h3" color="secondary">
-        Book Search
-      </Typography>
+         setResult(response.data.items);
+      } catch (error) {
+         window.alert("Please provide a valid book name");
+         window.location.replace("/search");
+      }
+   }
 
-      {/* //?Books Search */}
-      <form action="" onSubmit={handleClick}>
-        <TextField
-          label="Book Search"
-          variant="outlined"
-          onChange={e => setBook(e.target.value)}
-          value={book}
-          fullWidth
-        />
+   function handleClick(e) {
+      e.preventDefault();
+      loadData();
+   }
+   return (
+      <Container>
+         <Typography variant="h3" color="secondary">
+            Book Search
+         </Typography>
 
-        <Button
-          variant="contained"
-          color="success"
-          size="large"
-          onClick={handleClick}
-        >
-          Search
-        </Button>
-      </form>
+         {/* //?Books Search */}
+         <form action="" onSubmit={handleClick}>
+            <TextField
+               label="Book Search"
+               variant="outlined"
+               onChange={(e) => setBook(e.target.value)}
+               value={book}
+               fullWidth
+            />
 
-      {/* //*Books Information */}
-      <Grid container>
-        {result.map(book => {
-          try {
-            return (
-              <Details
-                key={book.id}
-                title={book.volumeInfo.title}
-                author={book.volumeInfo.authors}
-                image={book.volumeInfo.imageLinks.thumbnail}
-                description={book.volumeInfo.description
-                  .toString()
-                  .slice(0, 50)}
-                buy={book.saleInfo.buyLink}
-              />
-            );
-          } catch (err) {
-            <Details
-              key={book.id}
-              title={book.volumeInfo.title}
-              author={book.volumeInfo.authors}
-              image={""}
-              description={""}
-              buy={""}
-            />;
-          }
-        })}
-      </Grid>
-    </Container>
-  );
+            <Button variant="contained" color="success" size="large" onClick={handleClick}>
+               Search
+            </Button>
+         </form>
+
+         {/* //?Books Information */}
+         <Grid container direction="row" justifyContent="space-between" alignItems="baseline">
+            {result.map((book) => {
+               try {
+                  return (
+                     <Details
+                        key={book.id}
+                        title={book.volumeInfo.title}
+                        author={book.volumeInfo.authors}
+                        image={book.volumeInfo.imageLinks.thumbnail}
+                        description={book.volumeInfo.description}
+                        buy={book.saleInfo.buyLink}
+                        price={book.saleInfo.retailPrice.amount}
+                     />
+                  );
+               } catch (err) {
+                  return (
+                     <Details
+                        key={book.id}
+                        title={book.volumeInfo.title}
+                        author={book.volumeInfo.authors}
+                        image={book.volumeInfo.imageLinks.thumbnail}
+                        description={""}
+                        buy={""}
+                        price={""}
+                     />
+                  );
+               }
+            })}
+         </Grid>
+      </Container>
+   );
 }
 
 export default Search;
